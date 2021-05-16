78808d05408d11bf1cfc6e1a7123405dd2600652
Aga K <59511312+agnieszkaq@users.noreply.github.com>
2021-05-16 20:49:39 +0200

    Update PalindromeApplication.java

--- src/main/java/com/example/palindrome/PalindromeApplication.java
+++ src/main/java/com/example/palindrome/PalindromeApplication.java
@@ -8,6 +8,9 @@ public class PalindromeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PalindromeApplication.class, args);
+		System.out.print("Spring Boot Application is running!");
+		System.out.print("Write in browser, http://localhost:8080/isPalindrome?text=yourSpecyficWord");
+	
 	}
 
 }
